98cf57d52c89d127fa173be4916c249b75631ca7
NithinKumarAlladi <nithinkumaralladi.me@gmail.com>
2020-01-05 20:19:39 +0530

    configured for post request

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+var fs = require('fs');
+var util = require('util');
+var path = require('path');
+
 
 var server = app.listen(3001 , ()=>{console.log('Server is listening at port '+ server.address().port)});
 
@@ -7,9 +12,24 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 });
 app.get('/show',(req, res)=>{
-    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Origin','*');
+    console.log("request" + req);
     res.send({
         name:'Nithin kumar',
         surname:'Alladi'
     });
 });
+app.get('/users/:id',(req,res)=>{
+    var allUsers = [];
+    var filePath = path.join(__dirname,'/Data/allUsers.json');
+    fs.readFile(filePath,(err,data)=>{
+        // allUsers = util.format(data);
+        res.send(data);
+    })
+});
+
+app.post('/data',(req,res)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    console.log(req.body);
+    res.send('your data is stored');
+})
